ff987a608fa64150d0b97c72ac911fdb4a6532ff
saigopal-dev <saigopal.mepstra@gmail.com>
2024-12-05 14:35:44 +0530

    modified route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -10,7 +10,6 @@ import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 export async function POST(req: Request) {
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-
     if (!WEBHOOK_SECRET) {
         throw new Error(
             "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
@@ -18,7 +17,7 @@ export async function POST(req: Request) {
     }
 
     // Get the headers
-    const headerPayload = await headers();
+    const headerPayload = eheaders();
     const svix_id = headerPayload.get("svix-id");
     const svix_timestamp = headerPayload.get("svix-timestamp");
     const svix_signature = headerPayload.get("svix-signature");
@@ -69,13 +68,12 @@ export async function POST(req: Request) {
             lastName: last_name,
             photo: image_url,
         };
-        const clerk = await clerkClient();
 
         const newUser = await createUser(user);
 
         // Set public metadata
         if (newUser) {
-            await clerk.users.updateUserMetadata(id, {
+            await (await clerkClient()).users.updateUserMetadata(id, {
                 publicMetadata: {
                     userId: newUser._id,
                 },
