5ba103dfb6383253618bd5febf6324e9b4506ee2
saigopal-dev <saigopal.mepstra@gmail.com>
2024-12-05 14:43:48 +0530

    route.ts fix again

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
     }
 
     // Get the headers
-    const headerPayload = eheaders();
+    const headerPayload = headers();
     const svix_id = headerPayload.get("svix-id");
     const svix_timestamp = headerPayload.get("svix-timestamp");
     const svix_signature = headerPayload.get("svix-signature");
